28726acd8a1c4c38b876352ef2e1a3d5a1b5b2db
zhangmk <51670196@qq.com>
2018-11-14 15:08:47 +0800

    address demo update

--- api/pan_demo.py
+++ api/pan_demo.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import geo as g
 import json as json
-from db_connects_demo.db_connects import db 
+from db_connects.db_connects import db 
 import pandas as pd
 from math import radians, cos, sin, asin, sqrt
 from datetime import datetime
@@ -12,10 +12,10 @@ db = db()
 # from tools.icd import idcard_recognize 
 
 CURR_PATH = os.path.split(os.path.abspath(__file__))[0]
-SHOPS_WITHGEO = os.path.join(CURR_PATH,'../data/csv/shops_withgeo.csv')
-USERS_WITHGEO = os.path.join(CURR_PATH,'../data/csv/users_withgeo.csv')
-D_JSON = os.path.join(CURR_PATH,'../data/d.json')
-LINES_JSON = os.path.join(CURR_PATH,'../data/lines.json')
+SHOPS_WITHGEO = os.path.join(CURR_PATH,'../data_demo/csv/shops_withgeo_demo.csv')
+USERS_WITHGEO = os.path.join(CURR_PATH,'../data_demo/csv/users_withgeo_demo.csv')
+D_JSON = os.path.join(CURR_PATH,'../data_demo/d.json')
+LINES_JSON = os.path.join(CURR_PATH,'../data_demo/lines.json')
 
 def haversine(lon1, lat1, lon2, lat2): # 经度1，纬度1，经度2，纬度2 （十进制度数）
     """
@@ -56,20 +56,8 @@ def get_data(sqlname,id=0):
 
 def shop_transform():
     '''
-    商户geo数据构造 先从数据库读取数据，根据地址去外部api查地理位置，再计算转换成前端需要的json格式存成文件，返回值是dataframe给后续调用
-    geo的api调用很慢，采用增量模式，已经查过的shop不查
     ''' 
-    df_shops = get_data('shop')
-    df_shops = df_shops.set_index('shop_id')
-    # shops  = 'data/csv/shops.csv'
-    # df_shops = pd.read_csv(shops,encoding='utf-8',sep='>')
-    # # print(df_shops[['address','城市','company_registered_address']])
-    # 读取历史数据
-    df_shops_withgeo = pd.read_csv(SHOPS_WITHGEO, encoding='utf-8', sep=',',index_col='shop_id')
-    # 新增的商户数据   因为在生成shops_withgeo的时候会过滤掉地址为空的数据 因此每次这些数据会在new里面，不用管直接往下执行
-    df_shops_new = df_shops.loc[df_shops.index.difference(df_shops_withgeo.index)]
-    # print(df_shops_new)
-
+    df_shops_new = pd.read_csv(SHOPS_WITHGEO, encoding='utf-8', sep=',',index_col='shop_id')
     # exit()
     if len(df_shops_new)!=0:
         # 构造地址 采用城市+当前住址或公司地址
@@ -82,10 +70,10 @@ def shop_transform():
         # 调用api获取经纬度(读取shops_withgeo.csv判断是否有新增的机构，如果有调用get_geo)
         df_shops_new[['latitude','longitude']] = df_shops_new['address'].apply(lambda x: get_geo(x)).apply(pd.Series)
 
-    df_shops_res = pd.concat([df_shops_withgeo,df_shops_new],axis=0)
+    df_shops_res = df_shops_new
     #解决concat字段自动排序问题   https://github.com/pandas-dev/pandas/issues/4588
-    df_shops_res = df_shops_res.reindex(df_shops_withgeo.columns,axis=1)
-    df_shops_res.update(df_shops)
+    # df_shops_res = df_shops_res.reindex(df_shops_withgeo.columns,axis=1)
+    # df_shops_res.update(df_shops)
     # dtypes = df_shops_new.dtypes.combine_first(df_shops_withgeo.dtypes)
     # df_shops_res = df_shops_new.combine_first(df_shops_withgeo)
     # for k, v in dtypes.iteritems():
@@ -146,51 +134,27 @@ def shop_transform():
 
 def user_transform(df_shops_withgeo):
     '''
-    用户申请geo数据构造 先从数据库读取数据，根据地址去外部api查地理位置，再计算转换成前端需要的json格式存成文件，返回值是dataframe给后续调用
-    geo的api调用很慢，采用增量模式，已经查过的用户不查
     ''' 
     # 读取历史数据
-    df_users_withgeo = pd.read_csv(USERS_WITHGEO, encoding='utf-8', sep=',',index_col='id')
-    df_users = get_data('user',df_users_withgeo.index.max())
-    df_users_new = df_users.set_index('id')   # debit_id
-    # 全量初始化
-    # df_users.loc[:,'address'] = (df_users['area_name']+df_users['current_address'].fillna(df_users['company_address'])).fillna(df_users['idcard_area_name'])
-    # df_users[['longitude','latitude']] = df_users[:600]['address'].apply(lambda x: get_geo(x)).apply(pd.Series)
-    # df_users.to_csv('data/csv/users_withgeo.csv')
-    # exit()
-    # df_users_new = df_users.loc[df_users.index.difference(df_users_withgeo.index)]
+    df_users_new = pd.read_csv(USERS_WITHGEO, encoding='utf-8', sep=',',index_col='id')
     if len(df_users_new)!=0:
         # 地址用填写的城市名称+当前地址，如果为空用公司地址，都为空用身份证省所在省会城市
         df_users_new.loc[:,'address'] = (df_users_new['area_name']+df_users_new['current_address'].fillna(df_users_new['company_address'])).fillna(df_users_new['idcard_area_name'])
         df_users_new.loc[:,'address'] = df_users_new['address'].str.replace('\'','_').str.replace('"','_').str.replace('{','_').str.replace('}','_')
         df_users_new[['latitude','longitude']] = df_users_new['address'].apply(lambda x: get_geo(x)).apply(pd.Series)
-        print('new')
-    df_users_res = pd.concat([df_users_new,df_users_withgeo], axis=0)
+        # print('new')
+    # df_users_res = pd.concat([df_users_new,df_users_withgeo], axis=0)
     #解决concat字段自动排序问题   https://github.com/pandas-dev/pandas/issues/4588
-    df_users_res = df_users_res.reindex(df_users_withgeo.columns,axis=1)
+    # df_users_res = df_users_res.reindex(df_users_withgeo.columns,axis=1)
 
     # 计算datesince final_input_time:2018-08-28 17:11:12
-    df_users_res['final_input_time'] = pd.to_datetime(df_users_res['final_input_time'])
-    df_users_res.loc[:,'datesince'] = df_users_res['final_input_time'].apply(lambda x: (x.date() - datetime.today().date()).days)
+    df_users_new['final_input_time'] = pd.to_datetime(df_users_new['final_input_time'])
+    df_users_new.loc[:,'datesince'] = df_users_new['final_input_time'].apply(lambda x: (x.date() - datetime.today().date()).days)
     
     
     # df_users_res.update(df_users)
-    df_users_res.to_csv(USERS_WITHGEO)
-    '''
-    users_withgeo 处理成最终的json格式
-    {
-        "type": "FeatureCollection",
-        "features": [
-        {"type":"Feature","properties":{"fclass":"secondary","name":"West 106th Street","oneway":"B","bridge":"F","tunnel":"F"},"geometry":{"type":"LineString","coordinates":[[121.469560, 31.197440],[121.81700, 31.570000],[121.9910903,31.128485]]}},
-        {"type":"Feature","properties":{"fclass":"residential","name":"West 80th Street","oneway":"F","bridge":"F","tunnel":"F"},"geometry":{"type":"LineString","coordinates":[[121.469560, 31.197440],[121.465560, 31.193440]]}},
-        {"type":"Feature","properties":{"fclass":"residential","name":"West 84th Street","oneway":"F","bridge":"F","tunnel":"F"},"geometry":{"type":"LineString","coordinates":[[121.469560, 31.197440],[121.49788288,31.7875673]]}},
-        {"type":"Feature","properties":{"fclass":"residential","name":"Szold Place","oneway":"F","bridge":"F","tunnel":"F"},"geometry":{"type":"LineString","coordinates":[[121.49753143,31.7265446],[121.4975785,31.72592],[121.4976212,31.725326]]}},
-        {"type":"Feature","properties":{"fclass":"residential","name":"La Salle Street","oneway":"B","bridge":"F","tunnel":"F"},"geometry":{"type":"LineString","coordinates":[[121.49568143,31.8126066],[121.49573871,31.8128407],[121.49596274,31.8137561],[121.49596845,31.813781],[121.49598158,31.8138385],[121.4960687,31.814192]]}},
-        {"type":"Feature","properties":{"fclass":"unclassified","name":"West 9th Street","oneway":"F","bridge":"F","tunnel":"F"},"geometry":{"type":"LineString","coordinates":[[121.4995914,31.732847],[121.49990143,31.7341472]]}},
-        {"type":"Feature","properties":{"fclass":"service","name":"Patchin Place","oneway":"B","bridge":"F","tunnel":"F"},"geometry":{"type":"LineString","coordinates":[[121.4999248,31.734783],[121.49993706,31.7351890]]}}
-        ]
-    }
-    '''
+    df_users_new.to_csv(USERS_WITHGEO)
+  
     df_users_withgeo = pd.read_csv(USERS_WITHGEO, encoding='utf-8', sep=',')
 
     # print( len(df_users_withgeo[-df_users_withgeo['longitude'].isnull()]) )
@@ -233,7 +197,7 @@ def user_transform(df_shops_withgeo):
 
     with open(LINES_JSON,'w') as f:
         f.write(str(usres).replace('"',"_").replace('\'','"'))    # 先把双引号过滤掉  否则json会出错     py中dict转str是单引号转成js里的json需要转换成双引号
-    return df_users_res
+    return df_users_new
 
 def transform_one(debitid):
     '''
@@ -265,21 +229,10 @@ def run(debitid=0):
         return res_json
 
 def run_demo(user_name,gender,demo_address):
-    # time.sleep(0.5)
-    # res = transform_one(debitid)
-    # print(res)
-    # res = res.loc[debitid]
     geo = get_geo(demo_address)
     res_json = {"debitid":000,"shopid":000,"user_address":demo_address,"shop_address":demo_address,"final_input_time":"2018-11-12 18:00:00","abbreviation":"测试","user_name":user_name,"gender":gender,"age":"18","oper_nm":"测试测试","consumer_apply_amount":100000,"month_count":12,"grade_class":"A","coords":[[geo[1],geo[0]],[121.417475, 31.211993]]}
-    # print(res_json)
     return res_json
 
-if __name__ == '__main__':
-    # gg = get_geo('广东省揭阳市')
-    # # # gg = get_geo('广东省清远市英城街道富域城8栋701')
-    # print(gg)
-    # exit()
-    # run(debitid=19407239)
-   run()
-    # idcard_recognize.process('api/tools/icd/testimages/8nbSJPQIyEytk5G4FTseYMGRZRAXN3thllA0mFoiCnh2q7SspZBIT__amOyYsf13.jpg')
 
+if __name__ == '__main__':
+   run()
